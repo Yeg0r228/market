956867b176a19caabf18c7078db3c1b2ea5716fc
Yeg0r228 <24b_pyeo@liceum.ztu.edu.ua>
2025-05-05 15:52:15 +0300

    випрлено попердні недоліки,додано фото товарів,додано анімацію при наведенні еа ціну та на картинку.Додано JavaScript З функціоналом для анмацій.Заокруглено кути та додано тіні.Доведено до кінцевого результату

--- market.js
+++ market.js
@@ -1,6 +1,5 @@
 console.log('Підключено файл скриптів market.js');
 
-
 fetch('market.json') 
     .then(response => {
         if (!response.ok) {
@@ -9,22 +8,28 @@ fetch('market.json')
         return response.json();
     })
     .then(data => {
+        if (!data || data.length === 0) {
+            console.error('JSON-файл порожній або не містить даних');
+            return;
+        }
+
+        console.log('Завантажені дані:', data);
+
         data.forEach(item => {
-       
             let itemElement = document.getElementById(item.id);
             if (itemElement) {
-               
                 let titleElement = itemElement.querySelector(".item-title");
                 if (titleElement) {
                     titleElement.textContent = item.name;
                 }
 
-                
                 let imgElement = itemElement.querySelector("img");
                 if (imgElement) {
                     imgElement.src = item.image; 
                     imgElement.alt = item.name; 
                 }
+            } else {
+                console.warn(`Елемент із id=${item.id} не знайдено`);
             }
         });
     })
